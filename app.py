bd204dad340cceeefaac4e3cf31fd2e87e064fce
gorjessbbyx3 <jhoutz@hawaii.edu>
2025-05-08 07:59:00 +0000

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 """
 Main entry point for Dark Web Crawler application.
 """
@@ -6,4 +7,13 @@ from app.routes import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    host = os.environ.get('HOST', '0.0.0.0')
+    
+    # Configure app settings
+    app.config['HOST'] = host
+    app.config['PORT'] = port
+    app.config['OLLAMA_HOST'] = os.environ.get('OLLAMA_HOST', '0.0.0.0')
+    app.config['OLLAMA_PORT'] = int(os.environ.get('OLLAMA_PORT', 11434))
+    
+    # Run the application
+    app.run(host=host, port=port)
